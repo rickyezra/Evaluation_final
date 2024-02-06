5770f4865218b60a7af4761253efdb0639104fae
EZRA PANDITH <156065517+rickyezra@users.noreply.github.com>
2024-02-06 14:06:50 +0530

    Update add.c

--- add.c
+++ add.c
@@ -3,7 +3,7 @@
 int main() {
     int num1, num2, sum;
     
-    printf("Enter the first number: ");
+    printf("Enter the second number: ");
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
